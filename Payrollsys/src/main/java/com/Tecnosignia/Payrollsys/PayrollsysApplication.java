0f1f24a53eb0cd0eaeedc82a2ed13ad8af381426
prash <prash@HP>
2023-08-13 16:27:13 +0530

    First Commit...

---
+++ Payrollsys/src/main/java/com/Tecnosignia/Payrollsys/PayrollsysApplication.java
@@ -0,0 +1,13 @@
+package com.Tecnosignia.Payrollsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollsysApplication.class, args);
+	}
+
+}
---
+++ Payrollsys/src/test/java/com/Tecnosignia/Payrollsys/PayrollsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tecnosignia.Payrollsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
